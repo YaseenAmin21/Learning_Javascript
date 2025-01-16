06a29870641fa85ea29c6f5def074d9384de3f07
YaseenAmin21 <yaseenamindev@gmail.com>
2025-01-15 21:04:26 -0600

    date and time

---
+++ 01_Basics/07_datesinJS.js
@@ -0,0 +1,27 @@
+let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreateDate = new Date(2023, 0, 23, 5, 3);
+// let myCreateDate = new Date("2023-01-14");
+let myCreateDate = new Date("01-014-2023");
+
+// console.log(myCreateDate);
+// console.log(myCreateDate.toLocaleString());
+
+let myTimeStamp = Date.now();
+
+// console.log(myTimeStamp);
+// console.log(myCreateDate.getTime());
+// console.log(Math.floor(Date.now() / 1000));
+
+let newDate = new Date();
+
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getDay());
+
+newDate.toLocaleDateString("default", {
+  weekday: "long",
+});
