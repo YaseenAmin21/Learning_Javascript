20ed81e552cf239e166f182f9a76d270b38dace0
YaseenAmin21 <yaseenamindev@gmail.com>
2025-01-18 15:41:09 -0600

    Functions with objects and array

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -33,3 +33,33 @@ function loginUserMessage(username = "same") {
 }
 
 console.log(loginUserMessage("Yaseen"));
+
+function calCulateCartPrice(val1, val2, ...num1) {
+  return num1;
+}
+
+// console.log(calCulateCartPrice(200, 400, 500));
+
+const user = {
+  username: "Yaseen",
+  price: 199,
+};
+
+function handleObject({ username, price }) {
+  console.log(`Username is ${username} and price is ${price}`);
+}
+
+// handleObject(user);
+handleObject({
+  username: "YaseenAmin",
+  price: 399,
+});
+
+const myNewArray = [200, 400, 100, 600];
+
+function returnSecondValue(getArray) {
+  return getArray[1];
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
