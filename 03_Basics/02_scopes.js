01f93ac99e7df0cd8f7def3e8992775a24c0b33d
YaseenAmin21 <yaseenamindev@gmail.com>
2025-01-19 14:10:58 -0600

    Scope level and mini hoisting in javascript

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -9,9 +9,45 @@ if (true) {
   let a = 10;
   const b = 20;
   var c = 30;
-  console.log("INNER", a);
+  // console.log("INNER", a);
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
-console.log(c);
+// console.log(c);
+
+// Nested Scope
+function one() {
+  const username = "yaseen";
+  function two() {
+    const website = "youtube";
+    console.log(username);
+  }
+  // console.log(website);
+
+  two();
+}
+
+one();
+
+if (true) {
+  const username = "Amin";
+  if (username === "Amin") {
+    const website = "youtube";
+    console.log(username + website);
+  }
+  // console.log(website);
+}
+// console.log(username);
+
+// ++++++++++++++++ interesting ++++++++++++++++
+
+console.log(addone(5));
+function addone(num) {
+  return num + 1;
+}
+
+console.log(addTwo(5));
+const addTwo = function (num) {
+  return num + 2;
+};
