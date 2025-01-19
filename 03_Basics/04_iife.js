5331dfeb54ff273d659c4e84171bad45cca95fdf
YaseenAmin21 <yaseenamindev@gmail.com>
2025-01-19 16:13:17 -0600

    Immediately Invoked Function Expressions IIFE

---
+++ 03_Basics/04_iife.js
@@ -0,0 +1,11 @@
+(function () {
+  console.log("Yaseen");
+})();
+
+(function chai() {
+  console.log("DB Connected");
+})();
+
+((name) => {
+  console.log(`DB COnnected to ${name}`);
+})("MySql");
